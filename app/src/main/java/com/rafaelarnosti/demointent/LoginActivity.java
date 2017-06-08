20568dfe58ec151b4cc5fbf837c30725b680605b
Rafael Arnosti <rlm.arnosti@gmail.com>
2017-06-07 21:06:37 -0300

    Commit

--- app/src/main/java/com/rafaelarnosti/demointent/LoginActivity.java
+++ app/src/main/java/com/rafaelarnosti/demointent/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.rafaelarnosti.demointent;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+        import butterknife.BindView;
+        import butterknife.ButterKnife;
+        import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity  {
+
+    @BindView(R.id.etLogin)
+    EditText etLogin;
+    @BindView(R.id.etSenha)
+    EditText etSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.tvUsuario)
+    public void novoUsuarioClick(){
+        Intent novoUsuario = new Intent(this, NewUserActivity.class);
+        startActivityForResult(novoUsuario,1);
+    }
+
 }
--- app/src/main/java/com/rafaelarnosti/demointent/NewUserActivity.java
+++ app/src/main/java/com/rafaelarnosti/demointent/NewUserActivity.java
@@ -1,13 +1,35 @@
 package com.rafaelarnosti.demointent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NewUserActivity extends AppCompatActivity {
+
+    @BindView(R.id.etInformeUserName)
+    EditText etInformeUserName;
+
+    @BindView(R.id.etInformeName)
+    EditText etInformeName;
+
+    @BindView(R.id.etInformeSenha)
+    EditText etInformeSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btCriar)
+    public void criar(){
+        Intent i = new Intent();
+
     }
 }
