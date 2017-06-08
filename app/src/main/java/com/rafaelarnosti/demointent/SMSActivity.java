0f94efcc9a6b0f382bd4d130833c693862e6455f
Rafael Arnosti <rlm.arnosti@gmail.com>
2017-06-07 22:49:56 -0300

    Commit

--- app/src/main/java/com/rafaelarnosti/demointent/SMSActivity.java
+++ app/src/main/java/com/rafaelarnosti/demointent/SMSActivity.java
@@ -1,13 +1,63 @@
 package com.rafaelarnosti.demointent;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SMSActivity extends AppCompatActivity {
 
+    private BroadcastReceiver mReceiver;
+
+    private TextView tvMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+        tvMessage = (TextView) findViewById(R.id.tvMessage);
+
+        if(getIntent() != null) {
+            Intent i = getIntent();
+            String remetente = i.getStringExtra("remetente");
+            String mensagem = i.getStringExtra("mensagem");
+            tvMessage.setText(remetente == null && mensagem ==null ? "" : remetente + " : " + mensagem);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter(
+                "android.intent.action.SMSRECEBIDO");
+
+        mReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //extract our message from intent
+                String remetente = intent.getStringExtra("remetente");
+                String mensagem = intent.getStringExtra("mensagem");
+                tvMessage.setText(remetente + " : " + mensagem);
+
+            }
+        };
+        //registering our receiver
+        this.registerReceiver(mReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //unregister our receiver
+        this.unregisterReceiver(this.mReceiver);
     }
 }
